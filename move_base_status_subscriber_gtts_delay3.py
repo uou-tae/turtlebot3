2ee1a9ac54f7ca3c1c1bedf7f5777a2ff139f7ca
uou-tae <163511327+uou-tae@users.noreply.github.com>
2024-03-21 14:12:22 +0900

    Update move_base_status_subscriber_gtts_delay3.py

--- move_base_status_subscriber_gtts_delay3.py
+++ move_base_status_subscriber_gtts_delay3.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 
+#pip install playsound 해줘야함
+
 import rospy
+import time
 from actionlib_msgs.msg import GoalStatusArray
-#pip install gtts ==1.2.2
-#pip install playsound 위 두 문장 해줘야함
-from gtts import gTTS
 from playsound import playsound
+
+ audio = 'speech.mp3'
 
 def status_callback(msg):
     # 메시지를 처리하는 콜백 함수
     for status in msg.status_list:
         # status_list에 있는 각 status를 처리
         if status.status == 3:  # status가 3인 경우
-            print("ok")
+            print("goal reached")
+            print("Status: ", status.status)
         if status.status == 1 :
             print("working")
-
-            #gtts 사운드 재생
-            audio = 'speech.mp3'
-            language = 'ko'
-
-            sp = gTTS(
-            lang=language,
-            text="운행중입니다",
-            slow=False
-            )
-
-            time.sleep(3)  #3초 대기 후 다시 출력
+            print("Status: ", status.status)
+            playsound(audio)
             break  # 하나만 출력하고 종료
 
 def status_subscriber():
     rospy.init_node('status_subscriber', anonymous=True)
     rospy.Subscriber("/move_base/status", GoalStatusArray, status_callback)
-    rospy.spin()  # 노드가 종료될 때까지 실행 대기
+    rate = rospy.Rate(0.33)  # 3초에 한 번씩 메시지 처리
+    while not rospy.is_shutdown():
+        rospy.spinOnce()
+        try:
+            rate.sleep()
+        except rospy.exceptions.ROSTimeMovedBackwardsException:
+            pass
 
 if __name__ == '__main__':
     try:
