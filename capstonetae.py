0147214bcdb916c15e07e742cf4ad6a69db108b5
uou-tae <163511327+uou-tae@users.noreply.github.com>
2024-04-12 17:05:22 +0900

    Update capstonetae.py

--- capstonetae.py
+++ capstonetae.py
@@ -5,7 +5,7 @@ from actionlib_msgs.msg import GoalStatusArray
 
 # 초기 로봇의 위치를 저장하는 변수
 initial_pose = None
-
+work = 0
 def move_turtlebot3_to_goal(x, y):
     # 목표 위치를 설정한 PoseStamped 메시지 생성
     goal_pose = PoseStamped()
@@ -28,8 +28,8 @@ def status_callback(msg):
     global goal, work
     for status in msg.status_list:
         if status.status == 3:  # status가 3인 경우  goal reachead 인상태
-            # 초기 위치로 이동
-            move_turtlebot3_to_goal(0, 0)
+            work = work + 1      # work 1 , 2  반복
+            work = work % 2
             rate.sleep()
 
 def status_subscriber():
@@ -40,14 +40,18 @@ if __name__ == "__main__":
         rospy.init_node("SRL_targetpoint")
         goal_publisher = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
         rospy.Subscriber("/odom", Odometry, odom_callback)
-        status_subscriber()
-        rospy.sleep(1)  # Publisher가 초기화될 때까지 대기
 
         rate = rospy.Rate(1)  # 1Hz, adjust the rate as needed
+        if work == 0:
+            # 첫 번째 동작 목표 위치로 이동
+            move_turtlebot3_to_goal(-0.34728726744651794 ,1.197120189666748)
+        elif work == 1:
+            # 두 번째 동작 초기 위치로 이동
+            move_turtlebot3_to_goal(0, 0)
 
-        # 첫 번째 목표 위치로 이동
-        move_turtlebot3_to_goal(-0.34728726744651794 ,1.197120189666748)
         rate.sleep()
+        status_subscriber()
+        
         
     except rospy.ROSInterruptException:
         pass
